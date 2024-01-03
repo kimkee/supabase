5b752f4f8e6ddba6912ae792c2ef8867f0de76d6
KimKeeHyun <kimkee7@gmail.com>
2024-01-03 18:23:03 +0900

    CC

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          ee Get started by editing&nbsp;
+          GET STARTED BY EDITING&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
