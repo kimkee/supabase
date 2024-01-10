31c0d82064f58041bef78e28d92ba2ea34fe8af5
KimKeeHyun <kimkee7@gmail.com>
2024-01-10 10:50:36 +0900

    clsx(`li`,{ 'active': pathname.includes('products')})

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,15 +1,19 @@
 'use client'
 import React, {  useEffect,useState } from 'react'; //useState, useEffect
 // import {NavLink , useLocation } from 'react-router-dom'; // Link  , useLocation, useSearchParams,useParams, useSearchParams
-
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 import ui from '../ui.js';
 
 export default function Nav() {
   
   // const location = useLocation();
-  
+  const pathname = usePathname();
 
-  const isActive = els => location.pathname.includes(`${els}`) ? "active" : "";
+  // const isActive = els => location.pathname.includes(`${els}`) ? "active" : "";
+  const isActive = function(els)  {
+    console.log(EventTarget);
+  };
   
   // console.log( isActive() );
 
@@ -20,8 +24,6 @@ export default function Nav() {
   };
 
   const goTop = ()=> ui.scrollTo("body", 0 , 200 );
-
-
   const [userInfo, setUserInfo] = useState({});
   
   useEffect( () => {
@@ -36,10 +38,10 @@ export default function Nav() {
       <nav id="menubar" className="menubar">
         <div className="inr">
           <ul className="menu">
-            <li className={``}>
+            <li className={ clsx(`li`,{ 'active': pathname.includes('home')}) }>
               <a href={`/`} className={"bt"}><i className="fa-regular fa-house"></i><em>Home</em></a>
             </li>
-            <li className={``}>
+            <li className={ clsx(`li`,{ 'active': pathname.includes('products')}) }>
               <a href={`/products`} className={"bt"}><i className="fa-solid fa-box"></i><em>Products</em></a>
             </li>
             <li className={``}>
@@ -50,24 +52,6 @@ export default function Nav() {
               {/* <a href={`/user/signin`} className={"bt"}><i className="fa-regular fa-user"></i><em>Login</em></a> */}
             </li>
           </ul>
-          {/* <ul className="menu">
-            <li className={``}>
-              <NavLink to={`/home/`} className={"bt"}><i className="fa-regular fa-house"></i><em>Home</em></NavLink>
-            </li>
-            <li className={isActive("list/movie")}>
-              <NavLink to={`/list/movie/0/`} className={"bt"}><i className="fa-regular fa-clapperboard-play"></i><em>Movie</em></NavLink>
-            </li>
-            <li className={isActive("list/tv")}>
-              <NavLink to={`/list/tv/0/`} className={"bt"}><i className="fa-regular fa-tv-retro"></i><em>TV</em></NavLink>
-            </li>
-            <li className={isActive("search/movie")}>
-              <NavLink to={`/search/movie/`} className={"bt"}><i className="fa-regular fa-search"></i><em>Search</em></NavLink>
-            </li>
-            <li className={isActive("user/")}>
-              <NavLink to={`/user/${userInfo.uid}`} className={"bt"}> <i className="fa-regular fa-user"></i><em>Mypage</em></NavLink>
-              <NavLink to={`/user/signin`} className={"bt"}><i className="fa-regular fa-user"></i><em>Login</em></NavLink>
-            </li>
-          </ul> */}
         </div>
       </nav>
     </>
