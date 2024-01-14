2afc86c77d8a76444a78ec1c35e33491784884c6
KimKeeHyun <kimkee7@gmail.com>
2024-01-14 17:06:55 +0900

    manifest.js

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,24 +1,24 @@
 export default function manifest() {
-	return {
-	  name: 'SUPA MARKET',
-	  short_name: 'SUPA MARKET',
-	  description: 'SUPA MARKET + Supabase + Nest.js',
-	  start_url: '/',
-	  display: 'standalone',
-	  background_color: '#1a1a1a',
-	  theme_color: '#1a1a1a',
-	  icons: [
-		{
-		  src: '/favicon.ico',
-		  sizes: 'any',
-		  type: 'image/x-icon',
-		  sizes: "48x48"
-		},
-		{
-		  src: "/img/icon_app.png",
-		  type: "image/png",
-		  sizes: "512x512"
-		}
-	  ],
-	}
+  return {
+    name: 'SUPAMARKET',
+    short_name: 'SUPAMARKET',
+    description: 'SUPAMARKET + Supabase + Nest.js',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#1a1a1a',
+    theme_color: '#1a1a1a',
+    icons: [
+    {
+      src: '/favicon.ico',
+      sizes: 'any',
+      type: 'image/x-icon',
+      sizes: "48x48"
+    },
+    {
+      src: "/img/icon_app.png",
+      type: "image/png",
+      sizes: "512x512"
+    }
+    ],
   }
+}
