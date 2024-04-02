c236c29b88b3375c0e806429f98c04fc9b74308e
KimKeeHyun <kimkee7@gmail.com>
2024-04-02 09:51:14 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-	async redirects() {
-		return [
-			{
-				source: '/404',
-				destination: '/404', // 커스텀 404 페이지 경로
-				permanent: true,
-			},
-			{
-				source: '/',
-				destination: '/home', // Matched parameters can be used in the destination
-				permanent: true,
-			},
-		];
-	},
+  async redirects() {
+    return [
+      {
+        source: '/404',
+        destination: '/404', // 커스텀 404 페이지 경로
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: '/home', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ];
+  },
 
-	remotePatterns: [
-		{
-			// 도메인 이름
-			domain: "fonts.googleapis.com",
-			// 패턴
-			patterns: ["/earlyaccess/notosanskr.css"],
-		},
-	],
+  remotePatterns: [
+    {
+      // 도메인 이름
+      domain: "fonts.googleapis.com",
+      // 패턴
+      patterns: ["/earlyaccess/notosanskr.css"],
+    },
+  ],
 }
