24d4e3c864fff0f8f0dcee0943aba1f1b8be6f51
KimKeeHyun <kimkee7@gmail.com>
2024-04-25 18:19:25 +0900

    viewport

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -43,6 +43,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, shrink-to-fit=no, viewport-fit=cover" />
       <body className={`body ${noto_sans.className}`}>
 
         <div className="wrap">
