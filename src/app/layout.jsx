26352e2510aa2fc1140add272b29159d0b468b98
KimKeeHyun <kimkee7@gmail.com>
2024-04-30 10:34:53 +0900

    viewport

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -44,6 +44,7 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
+  minimumScale: 1,
   userScalable: false,
   viewportFit:'cover',
   shrinkToFit: false,
