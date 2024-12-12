60eee57427780fbd6383a126ba116ebe8614ab6b
KimKeeHyun <kimkee7@gmail.com>
2024-12-12 13:55:36 +0900

    Search

---
+++ src/app/search/page.jsx
@@ -0,0 +1,25 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import React, { useEffect, useRef } from 'react';
+import { supabase } from '@/app/supabase.js';
+import ui from '@/app/ui.js';
+
+export default function Search() {
+
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div className="container page">
+      <main className={`contents`}>
+
+        <h2>Search</h2>
+
+
+      </main>
+    </div>
+  )
+};
