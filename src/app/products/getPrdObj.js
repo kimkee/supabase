a6cd5dcb63d87bc182df6bd57c151e535b00f1e5
KimKeeHyun <kimkee7@gmail.com>
2024-12-12 12:31:28 +0900

    ${process.env.NEXT_PUBLIC_SITE_URL}

--- src/app/products/getPrdObj.js
+++ src/app/products/getPrdObj.js
@@ -3,13 +3,13 @@ const getPrdObj = async ()=>{
   const conditionObj = {}; 
   const locationObj = {}; 
   const statusObj = {}; 
-  await fetch('/api/condition').then( result => result.json()).then( result =>{
+  await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}api/condition`).then( result => result.json()).then( result =>{
     result.forEach(item => conditionObj[item.id] = item.condition);
   });
-  await fetch('/api/location').then( result => result.json()).then( result =>{
+  await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}api/location`).then( result => result.json()).then( result =>{
     result.forEach(item => locationObj[item.id] = item.location);
   });
-  await fetch('/api/status').then( result => result.json()).then( result =>{
+  await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}api/status`).then( result => result.json()).then( result =>{
     result.forEach(item => statusObj[item.id] = item.status);
   });
 
